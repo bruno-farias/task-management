53036f4d45d32f2531d93714697575c5c1d8b532
Bruno Farias <brunofarias1@gmail.com>
2020-11-08 18:06:56 +0200

    auth credentials validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(30)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password should have at least one uppercase letter, one lowecase letter and at least one number or special letter'
+  })
   password: string;
 }
